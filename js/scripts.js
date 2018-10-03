f319f3bd0c0c0e75c5fd72763969f6c04dacb36e
kasambuli <cynthiakasambuli@gmail.com>
2018-10-03 15:26:22 +0300

    add js folder with the javascript files

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// var ourRequest = new XMLHttpRequest();
+// ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+// ourRequest.onload = function() {
+//     console.log(ourRequest.responseText)
+// }
+
+
+// ourRequest.send();
+
+
+var ourRequest = new XMLHttpRequest();
+ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+ourRequest.onload = function() {
+    var ourData = JSON.parse(ourRequest.responseText);
+    console.log(ourData[0]);
+};
